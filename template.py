4716f2481b82976cfa2681f7a3ad034023477d67
sachin <adm88@SACHIN>
2022-06-30 20:54:39 +0530

    creating desired directories and files

---
+++ template.py
@@ -0,0 +1,26 @@
+import os
+
+dirs=[
+    os.path.join('action_critic','A2C'),
+    os.path.join('action_critic','A3C'),
+    'custom_environment',
+    'Q_learning',
+    'Deep_q_learning',
+    'DDPG',
+
+]
+
+for dirs_ in dirs:
+    os.makedirs(dirs_)
+    with open(os.path.join(dirs_,'.gitkeep'),'w') as f:
+        pass
+
+files=[
+    'READ.MD',
+    'requirement.txt',
+    '.gitignore',
+]
+
+for files_ in files :
+    with open(files_,'w') as f:
+        pass
